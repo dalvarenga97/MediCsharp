fd8ee89f11b963607ff99fe88f8f791a70d88e21
Rodrigo Matto <Rodrigodavid-@hotmail.com>
2019-05-19 18:52:58 -0400

    Final.

--- InterfazMediCsharp/frmreposo.cs
+++ InterfazMediCsharp/frmreposo.cs
@@ -81,13 +81,13 @@ namespace InterfazMediCsharp
             Reposo.AgregarReposo(reposo);
             MessageBox.Show("El reposo se guardó con Exito!!");
             LimpiarForm();
-            ActualizarDataGrid();
-            //dtgReposo.DataSource = null;
-            //cmbDoctor.SelectedItem = null;
-            //cmbPaciente.SelectedItem = null;
-            //dtpDesde.Value = System.DateTime.Now;
-            //dtpHasta.Value = System.DateTime.Now;
-            //reposo = new Reposo();
+            //ActualizarDataGrid();
+            dtgReposo.DataSource = null;
+            cmbDoctor.SelectedItem = null;
+            cmbPaciente.SelectedItem = null;
+            dtpDesde.Value = System.DateTime.Now;
+            dtpHasta.Value = System.DateTime.Now;
+            reposo = new Reposo();
         }
     }
 }
