bad148dd0acce10d7e3451f0cfff377af54545a8
Rodrigo Matto <Rodrigodavid-@hotmail.com>
2019-05-20 05:18:58 -0400

    Implementacion de Paciente en el reporte de la consulta

--- MediCsharp/Consulta.cs
+++ MediCsharp/Consulta.cs
@@ -44,7 +44,7 @@ namespace MediCsharp
 
         public override string ToString()
         {
-            return this.NumeroConsulta + " " + NombreDoctor;
+            return this.NumeroConsulta + "      " + NombreDoctor + "      " + NombrePaciente;
         }
 
 
