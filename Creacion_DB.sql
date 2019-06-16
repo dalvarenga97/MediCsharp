0de85da49e26c8085451ca62e1794c1a636a2668
Rodrigo Matto <Rodrigodavid-@hotmail.com>
2019-06-16 17:26:56 -0400

    Errores simples.

--- Creacion_DB.sql
+++ Creacion_DB.sql
@@ -11,7 +11,7 @@ FILEGROUP TABLAS (name='DataFile_MediSharp_2',
 					FILEGROWTH = 10%)
 
 LOG ON  (name='LogFile_MediSharp_1',
-		filename='C:\SQL20198\LogFile_MediSharp_1.ldf',
+		filename='C:\SQL2019\LogFile_MediSharp_1.ldf',
 		SIZE = 100MB,
 		MAXSIZE = UNLIMITED,
 		FILEGROWTH = 10%);
@@ -32,7 +32,7 @@ CREATE TABLE Paciente
 	  Estado_Civil varchar(255),
 	  CONSTRAINT PK_Ciudad PRIMARY KEY NONCLUSTERED (CI_Paciente),
 	  CONSTRAINT CHK_Paciente_Sexo check (Sexo in ('Masculino', 'Femenino')),
-	  CONSTRAINT CHK_Paciente_Sexo check (Sexo in ('Solter@', 'Casad@')));
+	  CONSTRAINT CHK_Paciente_Estado_Civil check (Estado_Civil in ('Solter@', 'Casad@')));
 	  
 CREATE TABLE Doctor
 	 (Matricula varchar(255),
@@ -87,16 +87,16 @@ CREATE TABLE Consulta
 	  Diagnostico varchar(255),
 	  Tipo_Urgencia varchar(255),
 	  CONSTRAINT PK_Consulta PRIMARY KEY NONCLUSTERED (Numero_Consulta),
-	  CONSTRAINT FK_Consulta_Nombre_Doctor FOREIGN KEY (Nombre_Doctor) REFERENCES Doctor(Nombre),
-	  CONSTRAINT FK_Consulta_Nombre_Paciente FOREIGN KEY (Nombre_Paciente) REFERENCES Paciente(Nombre),					
+	  CONSTRAINT FK_Consulta_Nombre_Doctor FOREIGN KEY (Doctor) REFERENCES Doctor, --(Nombre),
+	  CONSTRAINT FK_Consulta_Nombre_Paciente FOREIGN KEY (Paciente) REFERENCES Paciente, --(Nombre),					
 	  CONSTRAINT FK_Consulta_Sucursal FOREIGN KEY (NroSucursal) REFERENCES Sucursal,
 	  CONSTRAINT CHK_Consulta_Tipo_Urgencia check (Tipo_Urgencia in ('Critico', 'Grave', 'Leve')));
 
 CREATE TABLE DetalleMedicamento
 	 (Codigo_Medicamento int,
-	  Numero_Consulta datetime,
+	  Numero_Consulta int,
 	  Cantidad int,
-	  CONSTRAINT PK_DetalleMedicamento PRIMARY KEY NONCLUSTERED (Codigo_Reposo),						
+	  CONSTRAINT PK_DetalleMedicamento PRIMARY KEY NONCLUSTERED (Codigo_Medicamento, Numero_Consulta),						
 	  CONSTRAINT FK_DetalleMedicamento_Medicamento FOREIGN KEY (Codigo_Medicamento) REFERENCES Medicamento,
 	  CONSTRAINT FK_DetalleMedicamento_Consulta FOREIGN KEY (Numero_Consulta) REFERENCES Consulta);
 
